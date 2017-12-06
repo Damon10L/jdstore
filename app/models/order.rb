75137848d69cc980c63c70bf6e57fd1e1e90b57d
gzn <gzndamon@gmail.com>
2017-12-06 19:25:50 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validate :billing_name, presence: true
+  validate :billing_address, presence: true
+  validate :shipping_name, presence: true
+  validate :shipping_address, presence: true
+  
 end
